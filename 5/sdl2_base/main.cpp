78156e2991bd25148708ab1dad4aecf12b57e504
supersliser <thomaslower9@gmail.com>
2024-10-28 21:58:26 +0000

    dff

--- 5/sdl2_base/main.cpp
+++ 5/sdl2_base/main.cpp
@@ -40,6 +40,7 @@ typedef struct cam
 	float fov;
 	float nearClip;
 	float farClip;
+	bool orthographic;
 } cam;
 
 float dp(point p1, vec p2)
@@ -59,11 +60,32 @@ float dp(vec p1, vec p2)
 	return p1.x * p2.x + p1.y * p2.y + p1.z * p2.z;
 }
 
-void sortObjects(sphere *objs, int objCount)
+void sortObjects(sphere *objs, int* objCount, cam* c)
 {
-	for (int i = 0; i < objCount; i++)
+	int* toDelete = (int*)calloc(0, sizeof(int));
+	int toDeleteSize = 0;
+	for (int i = 0; i < *objCount; i++) {
+		if (objs[i].pos.z - objs[i].r < (*c).pos.z + (*c).nearClip && objs[i].pos.z - objs[i].r > (*c).pos.z + (*c).farClip) {
+			toDeleteSize++;
+			toDelete = (int*)realloc(toDelete, toDeleteSize * sizeof(int));
+			toDelete[toDeleteSize - 1] = i;
+
+		}
+	}
+
+	for (int i = 0; i < toDeleteSize; i++) {
+		// printf("%d\n", i);
+		fflush(stdout);
+		for (int j = 0; j < *objCount; j++) {
+			objs[i] = objs[j + i];
+		}
+		*objCount -= 1;
+		objs = (sphere*)realloc(objs, *objCount * sizeof(sphere));
+
+	}
+	for (int i = 0; i < *objCount; i++)
 	{
-		for (int j = i + 1; j < objCount; j++)
+		for (int j = i + 1; j < *objCount; j++)
 		{
 			if (objs[i].pos.z < objs[j].pos.z)
 			{
@@ -139,7 +161,7 @@ void draw(SDL_Renderer *ren, cam c, sphere *objs, int objCount)
 {
 	SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
 	SDL_RenderClear(ren);
-
+	sortObjects(objs, &objCount, &c);
 	for (int x = -500; x < 500; x++)
 	{
 		for (int y = -500; y < 500; y++)
@@ -150,38 +172,42 @@ void draw(SDL_Renderer *ren, cam c, sphere *objs, int objCount)
 			o.y = y + c.pos.y;
 			o.z = c.pos.z;
 			vec d;
-			d.x = (x) * (c.fov / 90);
-			d.y = (y) * (c.fov / 90);
-			d.z = 1000 * (c.fov / 90);
-			d = Normalise(d);
-			// printf("(%f, %f, %f)\n", d.x, d.y, d.z);
-			hit bestHit;
-			bestHit.hit = 0;
-			bestHit.dist = 100000000;
-			for (int i = 0; i < objCount; i++)
-			{
-				if (objs[i].pos.z > c.pos.z + c.nearClip && objs[i].pos.z < c.pos.z + c.farClip)
-				{
 
+			if (c.orthographic)
+			{
+				d.x = 0;
+				d.y = 0;
+				d.z = 1;
+			}
+			else
+			{
+				d.x = (x + c.dir.x) * (c.fov / 90);
+				d.y = (y + c.dir.y) * (c.fov / 90);
+				d.z = (500 + c.dir.z) * (c.fov / 90);
+				d = Normalise(d);
+			}
+			// printf("(%f, %f, %f)\n", d.x, d.y, d.z);
+			// hit bestHit;
+			// bestHit.hit = 0;
+			// bestHit.dist = 100000000;
+			for (int i = objCount; i > 0; i--)
+			{
 					hit h = traceObj(o, d, objs[i]);
 					if (h.hit)
 					{
-						if (h.dist < bestHit.dist)
-						{
-							bestHit = h;
-						}
+						// printf("%d\n", i);
+						float normalM = getNormal(h.P, h.obj, d);
+						SDL_SetRenderDrawColor(ren, floor(h.obj.col.r * 255) * normalM, floor(h.obj.col.g * 255) * normalM, floor(h.obj.col.b * 255) * normalM, 255);
+						SDL_RenderDrawPoint(ren, x + 500, y + 500);
+						break;
 					}
-				}
-			}
-			if (bestHit.hit)
-			{
-				float normalM = getNormal(bestHit.P, bestHit.obj, d);
-				SDL_SetRenderDrawColor(ren, floor(bestHit.obj.col.r * 255) * normalM, floor(bestHit.obj.col.g * 255) * normalM, floor(bestHit.obj.col.b * 255) * normalM, 255);
-				SDL_RenderDrawPoint(ren, x + 500, y + 500);
 			}
 		}
 	}
+	// printf("drawn");
+	fflush(stdout);
 	SDL_RenderPresent(ren);
+	// printf("drawn");
 }
 int main(int argc, char *argv[])
 {
@@ -208,43 +234,46 @@ int main(int argc, char *argv[])
 	c.dir.x = 0;
 	c.dir.y = 0;
 	c.dir.z = 1;
-	c.fov = -70;
+	c.fov = 20;
 	c.nearClip = 1;
-	c.farClip = 1000;
+	c.farClip = 1000000;
+	c.orthographic = false;
 
-	sphere *objs = (sphere *)malloc(sizeof(sphere) * 20);
-	objs[0].pos.x = 0;
-	objs[0].pos.y = 0;
-	objs[0].pos.z = 0;
-	objs[0].col.r = 1;
-	objs[0].col.g = 0;
-	objs[0].col.b = 0;
-	objs[0].r = 200;
-	objs[1].pos.x = -200;
-	objs[1].pos.y = 0;
-	objs[1].pos.z = 500;
-	objs[1].col.r = 1;
-	objs[1].col.g = 0;
-	objs[1].col.b = 1;
-	objs[1].r = 100;
+	int objectCount = 20;
 
-	sortObjects(objs, 2);
 
-	draw(ren, c, objs, 2);
-	for (int i = 2; i < 20; i++)
+	sphere *temp = (sphere *)malloc(sizeof(sphere) * objectCount);
+	
+	temp[0].pos.x = 0;
+	temp[0].pos.y = 0;
+	temp[0].pos.z = 0;
+	temp[0].col.r = 1;
+	temp[0].col.g = 0;
+	temp[0].col.b = 0;
+	temp[0].r = 200;
+	temp[1].pos.x = -200;
+	temp[1].pos.y = 0;
+	temp[1].pos.z = 500;
+	temp[1].col.r = 1;
+	temp[1].col.g = 0;
+	temp[1].col.b = 1;
+	temp[1].r = 100;
+
+
+	for (int i = 2; i < objectCount; i++)
 	{
-		objs[i].pos.x = (rand() % 2000) - 500;
-		objs[i].pos.y = (rand() % 2000) - 500;
-		objs[i].pos.z = (rand() % 2000) - 1000;
-		objs[i].col.r = ((float)(rand() % 155) + 100) / 255;
-		objs[i].col.g = ((float)(rand() % 155) + 100) / 255;
-		objs[i].col.b = ((float)(rand() % 155) + 100) / 255;
-		objs[i].r = (rand() % 100) + 50;
+		temp[i].pos.x = (rand() % 2000) - 500;
+		temp[i].pos.y = (rand() % 2000) - 500;
+		temp[i].pos.z = (rand() % 2000) - 1000;
+		temp[i].col.r = ((float)(rand() % 155) + 100) / 255;
+		temp[i].col.g = ((float)(rand() % 155) + 100) / 255;
+		temp[i].col.b = ((float)(rand() % 155) + 100) / 255;
+		temp[i].r = (rand() % 100) + 50;
 		// printf("(%f, %f, %f)\n", objs[i].col.r, objs[i].col.g, objs[i].col.b);
 		// printf("draw %d object\n", i + 1);
 	}
-	sortObjects(objs, 20);
-	draw(ren, c, objs, 20);
+	sphere **objs = &temp;
+	draw(ren, c, *objs, objectCount);
 	char finished = 0;
 	// the main event loop
 	while (!finished)
@@ -258,13 +287,13 @@ int main(int argc, char *argv[])
 				switch (event.key.keysym.sym)
 				{
 				case SDLK_w:
-					c.pos.z += MOVE * 10;
+					c.pos.z += MOVE;
 					break;
 				case SDLK_a:
 					c.pos.x -= MOVE;
 					break;
 				case SDLK_s:
-					c.pos.z -= MOVE * 10;
+					c.pos.z -= MOVE;
 					break;
 				case SDLK_d:
 					c.pos.x += MOVE;
@@ -284,8 +313,11 @@ int main(int argc, char *argv[])
 				case SDLK_RIGHTBRACKET:
 					c.fov += 10;
 					break;
+				case SDLK_p:
+					c.orthographic = !c.orthographic;
+					break;
 				}
-				draw(ren, c, objs, 20);
+				draw(ren, c, *objs, objectCount);
 				break;
 
 			case SDL_QUIT:
