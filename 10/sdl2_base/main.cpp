b8c339ce448447c0e70dedc28b08fdaae1d246b1
supersliser <thomaslower9@gmail.com>
2024-12-05 12:58:30 +0000

    weee

--- 10/sdl2_base/main.cpp
+++ 10/sdl2_base/main.cpp
@@ -2,23 +2,23 @@
 #include <math.h>
 #include <time.h>
 
-float f(float x)
+double f(float x)
 {
 	return sin(x) / x;
 }
 
-float pdf(float x)
+double pdf(double x)
 {
 	float v = 1 - (x / M_PI);
 	return v * (2 / M_PI);
 }
 
-float getPDFMax(int aCount)
+double getPDFMax(int aCount)
 {
-	float max = 0;
-	for (float i = 0; i < M_PI; i+= 0.01f)
+	double max = 0;
+	for (double i = 0; i < M_PI; i+= 0.01f)
 	{
-		float v = pdf(i);
+		double v = pdf(i);
 		if (v > max)
 			max = v;
 	}
@@ -29,25 +29,25 @@ int main(int argc, char *argv[])
 {
 	srandom(time(NULL));
 
-	float rt = 0;
+	double rt = 0;
 	const int aCount = 100000;
 
 	for (int i = 1; i < aCount; i++)
 	{
-		float x = ((float)rand() / (float)RAND_MAX) * M_PI;
+		double x = ((double)rand() / (double)RAND_MAX) * M_PI;
 		// printf("Random number: %f\n", randomNum);
-		float px = pdf(x);
-		float rn2 = MAXFLOAT;
+		double px = pdf(x);
+		double rn2 = MAXFLOAT;
 		while (rn2 > px)
 		{
-			rn2 = ((float)rand() / (float)RAND_MAX) * getPDFMax(aCount);
+			rn2 = ((double)rand() / (double)RAND_MAX) * getPDFMax(aCount);
 		}
 		rt += f(x) / px;
 
 		printf("y = %f\n", rt / i);
 	}
 
-	float out = rt / (aCount);
+	double out = rt / (aCount);
 	printf("final y = %f\n", out);
 
 	return 0;
---
+++ 11/sdl2_base/main.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <SDL2/SDL.h>
+
+
+int middleSquare(int x)
+{
+    int squared = x * x;
+    int squared_str_len = snprintf(NULL, 0, "%d", squared);
+    char *squared_str = (char *)malloc(squared_str_len + 1);
+    snprintf(squared_str, squared_str_len + 1, "%d", squared);
+
+    int start = (squared_str_len - 2) / 2;
+    int end = start + 2;
+
+    int middle_two_digits = atoi(squared_str + start) % 100;
+
+    free(squared_str);
+
+	printf("%d\n", middle_two_digits);
+
+    return middle_two_digits;
+}
+
+int lcg(int a, int m, int c, int x) {
+	int out = (a * x + c) % m;
+
+	printf("%f\n", out);
+	return out;
+}
+
+int bbs(int x, int p1, int p2) {
+	int m = p1 * p2;
+
+	int out = (x * x) % m;
+	printf("%d\n", out);
+	return out;
+}
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window *window = SDL_CreateWindow("Random", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 1000, 0);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	const int start = 0;
+
+	for (int x = start; x < 1000 + start; x++) {
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		// SDL_RenderDrawPoint(renderer, x - start, 1000 - ((lcg(8121, 134456, 28411, x)) % 1000));
+		// SDL_RenderDrawPoint(renderer, x - start, 1000 - ((middleSquare(x)) % 1000));
+		SDL_RenderDrawPoint(renderer, x - start, 1000 - ((bbs(x, 107, 191)) % 1000));
+
+	}
+
+	SDL_RenderPresent(renderer);
+
+	while (1)
+	{
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				SDL_Quit();
+				return 0;
+			}
+		}
+	}
+}
