62356a956f0569ab2c738627255afd2966a4d419
supersliser <thomaslower9@gmail.com>
2025-01-21 18:49:47 +0000

    wee

--- final/sdl2_base/main.cpp
+++ final/sdl2_base/main.cpp
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+//utility function to save images
 void saveImage(SDL_Renderer *ren, char *filename, int width, int height)
 {
 	SDL_Surface *sectionSurface = SDL_CreateRGBSurface(0, width, height, 32, 0xff000000, 0x00ff0000, 0x0000ff00, 0x000000ff);
@@ -15,6 +17,7 @@ void saveImage(SDL_Renderer *ren, char *filename, int width, int height)
 	fflush(stdout);
 }
 
+//basic color struct with alpha component
 typedef struct color
 {
 	Uint8 r;
@@ -23,6 +26,7 @@ typedef struct color
 	Uint8 a;
 } color;
 
+//utility function to read the colour of a given pixel from the screen
 color readPixel(SDL_Renderer *ren, int x, int y)
 {
 	SDL_Rect rect = {x, y, 1, 1};
@@ -35,8 +39,10 @@ color readPixel(SDL_Renderer *ren, int x, int y)
 	return c;
 }
 
+//discrete fourier transform for a given row of pixels
 float _Complex *ft(float _Complex *input, int y, int width)
 {
+	//data is kept separate to avoid contamination
 	float _Complex data[width];
 	float _Complex result[width];
 
@@ -56,6 +62,7 @@ float _Complex *ft(float _Complex *input, int y, int width)
 		result[x] /= width;
 	}
 
+	//output is placed onto the heap to avoid stack overflow and allow access to the array outside of the function without having to return all values.
 	float _Complex *result2 = (float _Complex *)malloc(sizeof(float _Complex) * width);
 
 	for (int x = 0; x < width; x++)
@@ -66,6 +73,7 @@ float _Complex *ft(float _Complex *input, int y, int width)
 	return result2;
 }
 
+//inverse discrete fourier transform for a given row of pixels
 float _Complex *Ift(float _Complex *input, int y, int width)
 {
 	float _Complex data[width];
@@ -111,16 +119,24 @@ int main(int argc, char *argv[])
 									   SDL_WINDOWPOS_CENTERED,
 									   SDL_WINDOWPOS_CENTERED,
 									   SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-
+	//create and clear screen
 	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, 0);
 	SDL_RenderClear(ren);
 	SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
 
+
+
+
+	//load image
 	SDL_Surface *image = IMG_Load("start_image.bmp");
 	SDL_Texture *texture = SDL_CreateTextureFromSurface(ren, image);
 	SDL_RenderCopy(ren, texture, NULL, NULL);
 	SDL_RenderPresent(ren);
 
+
+
+
+	//convert image to grayscale
 	for (int y = 0; y < SCREEN_HEIGHT; y++)
 	{
 		for (int x = 0; x < SCREEN_WIDTH; x++)
@@ -131,20 +147,26 @@ int main(int argc, char *argv[])
 			SDL_RenderDrawPoint(ren, x, y);
 		}
 	}
-
 	SDL_RenderPresent(ren);
 	saveImage(ren, "./grayscale_image.bmp", SCREEN_WIDTH, SCREEN_HEIGHT);
 
+
+
+
+	//fourier transform
 	float _Complex *result[SCREEN_HEIGHT];
 	for (int y = 0; y < SCREEN_HEIGHT; y++)
 	{
+		//generate array
 		float _Complex *data = (float _Complex *)malloc(sizeof(float _Complex) * SCREEN_WIDTH);
 		for (int x = 0; x < SCREEN_WIDTH; x++)
 		{
 			data[x] = readPixel(ren, x, y).r / 255.0;
 		}
+		//perform fourier transform on generated array
 		result[y] = ft(data, y, SCREEN_WIDTH);
 	}
+	//display result and save to file
 	for (int y = 0; y < SCREEN_HEIGHT; y++)
 	{
 		for (int x = 0; x < SCREEN_WIDTH; x++)
@@ -165,9 +187,13 @@ int main(int argc, char *argv[])
 	SDL_RenderPresent(ren);
 	saveImage(ren, "./fourier_image.bmp", SCREEN_WIDTH, SCREEN_HEIGHT);
 
+
+	//clear screen
 	SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
 	SDL_RenderClear(ren);
 
+
+	//flip the fourier transform between columns and rows
 	for (int y = 0; y < SCREEN_HEIGHT; y++)
 	{
 		for (int x = 0; x < SCREEN_WIDTH; x++)
@@ -176,6 +202,8 @@ int main(int argc, char *argv[])
 		}
 	}
 
+
+	//perform inverse fourier transform
 	float _Complex *result2[SCREEN_HEIGHT];
 	for (int y = 0; y < SCREEN_HEIGHT; y++)
 	{
@@ -198,33 +226,42 @@ int main(int argc, char *argv[])
 			SDL_RenderDrawPoint(ren, x, y);
 		}
 	}
-
 	SDL_RenderPresent(ren);
-
 	saveImage(ren, "./double_fourier_image.bmp", SCREEN_WIDTH, SCREEN_HEIGHT);
 
+
+	//clear screen
 	SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
 	SDL_RenderClear(ren);
 
+
+	//generate kernals for convolution
 	SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
 
-	SDL_Rect rect = {SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
+	bool const usingBoxKernal = false;
 
-	// const int borderSize = 50;
-
-	// SDL_Rect rect = {0, 0, borderSize, SCREEN_HEIGHT};
-	SDL_RenderFillRect(ren, &rect);
-	// SDL_Rect rect2 = {SCREEN_WIDTH - borderSize, 0, borderSize, SCREEN_HEIGHT};
-	// SDL_RenderFillRect(ren, &rect2);
-	// SDL_Rect rect3 = {0, 0, SCREEN_WIDTH, borderSize};
-	// SDL_RenderFillRect(ren, &rect3);
-	// SDL_Rect rect4 = {0, SCREEN_HEIGHT -borderSize, SCREEN_WIDTH, borderSize};
-	// SDL_RenderFillRect(ren, &rect4);
-
+	if (usingBoxKernal)
+	{
+		SDL_Rect rect = {SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
+		SDL_RenderFillRect(ren, &rect);
+	}
+	else
+	{
+		SDL_Rect rect = {0, 0, 50, SCREEN_HEIGHT};
+		SDL_RenderFillRect(ren, &rect);
+		SDL_Rect rect2 = {SCREEN_WIDTH - 50, 0, 50, SCREEN_HEIGHT};
+		SDL_RenderFillRect(ren, &rect2);
+		SDL_Rect rect3 = {0, 0, SCREEN_WIDTH, 50};
+		SDL_RenderFillRect(ren, &rect3);
+		SDL_Rect rect4 = {0, SCREEN_HEIGHT - 50, SCREEN_WIDTH, 50};
+		SDL_RenderFillRect(ren, &rect4);
+	}
 	SDL_RenderPresent(ren);
-
 	saveImage(ren, "./rect_image.bmp", SCREEN_WIDTH, SCREEN_HEIGHT);
 
+
+
+	//perform fourier transform on kernal
 	float _Complex *box_result[SCREEN_HEIGHT];
 	for (int y = 0; y < SCREEN_HEIGHT; y++)
 	{
@@ -235,7 +272,6 @@ int main(int argc, char *argv[])
 		}
 		box_result[y] = ft(data, y, SCREEN_WIDTH);
 	}
-
 	for (int y = 0; y < SCREEN_HEIGHT; y++)
 	{
 		for (int x = 0; x < SCREEN_WIDTH; x++)
@@ -253,11 +289,11 @@ int main(int argc, char *argv[])
 			SDL_RenderDrawPoint(ren, x, y);
 		}
 	}
-
 	SDL_RenderPresent(ren);
-
 	saveImage(ren, "./fourier_rect_image.bmp", SCREEN_WIDTH, SCREEN_HEIGHT);
 
+
+	//multiply the two fourier transforms to simulate convolution
 	float _Complex *convolution_result[SCREEN_HEIGHT];
 	for (int y = 0; y < SCREEN_HEIGHT; y++) {
 		convolution_result[y] = (float _Complex*)malloc(sizeof(float _Complex) * SCREEN_WIDTH);
@@ -265,11 +301,12 @@ int main(int argc, char *argv[])
 			convolution_result[y][x] = box_result[y][x] * result2[y][x];
 		}
 	}
-
 	SDL_RenderPresent(ren);
-
 	saveImage(ren, "./convolution_image.bmp", SCREEN_WIDTH, SCREEN_HEIGHT);
 
+
+	
+	//perform inverse fourier transform on the convolution result
 	float _Complex *Ift_result[SCREEN_HEIGHT];
 	for (int y = 0; y < SCREEN_HEIGHT; y++)
 	{
@@ -296,16 +333,9 @@ int main(int argc, char *argv[])
 	saveImage(ren, "./inverse_fourier_image.bmp", SCREEN_WIDTH, SCREEN_HEIGHT);
 
 
+
+	//perform inverse fourier transform on the inverse fourier transform result to return to true image
 	float _Complex *Ift_result2[SCREEN_HEIGHT];
-
-		for (int y = 0; y < SCREEN_HEIGHT; y++)
-	{
-		for (int x = 0; x < SCREEN_WIDTH; x++)
-		{
-			Ift_result[y][x] = Ift_result[y][x];
-		}
-	}
-
 	SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
 	SDL_RenderClear(ren);
 
@@ -333,6 +363,8 @@ int main(int argc, char *argv[])
 	SDL_RenderPresent(ren);
 	saveImage(ren, "./double_inverse_fourier_image.bmp", SCREEN_WIDTH, SCREEN_HEIGHT);
 
+
+	//wait for user to close window
 	while (1)
 	{
 		SDL_Event e;
@@ -346,6 +378,16 @@ int main(int argc, char *argv[])
 	}
 
 	saveImage(ren, "./end_image.bmp", SCREEN_WIDTH, SCREEN_HEIGHT);
+	
+	// Free allocated memory
+	for (int y = 0; y < SCREEN_HEIGHT; y++) {
+		free(result[y]);
+		free(result2[y]);
+		free(box_result[y]);
+		free(convolution_result[y]);
+		free(Ift_result[y]);
+		free(Ift_result2[y]);
+	}
 	SDL_DestroyTexture(texture);
 	SDL_FreeSurface(image);
 	SDL_DestroyRenderer(ren);
